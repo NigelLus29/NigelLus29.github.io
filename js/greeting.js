491e00b15bdb94429ee4e14bbb322c93e765ccf3
Nigel-Liao <nigelliao123@gmail.com>
2023-03-06 14:08:03 +0800

    modified:   index.html 	modified:   js/greeting.js 	modified:   js/theme.js 	modified:   js/time.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = " Nigel Lus 29 ";
+const name = " Nigel Liao";
 
 // Here you can change your greetings
 const gree1 = "Go to Sleep!  ";
@@ -13,11 +13,11 @@ const gree4 = "Good evening,  ";
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById("greetings").innerText = gree1 + name;
+  document.getElementById("greetings").innerText = gree1 + name + ".";
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById("greetings").innerText = gree2 + name;
+  document.getElementById("greetings").innerText = gree2 + name + ".";
 } else if (hour >= 12 && hour < 19) {
-  document.getElementById("greetings").innerText = gree3 + name;
+  document.getElementById("greetings").innerText = gree3 + name + ".";
 } else {
-  document.getElementById("greetings").innerText = gree4 + name;
+  document.getElementById("greetings").innerText = gree4 + name + ".";
 }
